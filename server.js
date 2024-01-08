f7e500f135f0fd1e96ed49cc5da1b360fba7ffa8
jproff <jessicakproffitt@gmail.com>
2024-01-08 18:42:29 -0500

    file reorg

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const routes = require('./Develop/routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Force false so data doesn't get dropped on every sync
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log('Now listening'));
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
